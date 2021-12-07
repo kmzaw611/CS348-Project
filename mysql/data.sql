0cfb6bf9ba90b8876614f40b562d880140aa7a43
Kaung Myat Zaw <zaw@purdue.edu>
2021-12-07 00:28:03 -0500

    Stored procedure for adding a game

---
+++ mysql/data.sql
@@ -0,0 +1,18 @@
+USE stugamez;
+
+CALL add_game(
+	"Red Dead Redepmtion 2",
+    59.99,
+    "Rockstar Games",
+    "https://www.youtube.com/watch?v=i3861v5e3HI",
+    "https://upload.wikimedia.org/wikipedia/en/4/44/Red_Dead_Redemption_II.jpg",
+    93,
+    44,
+    6.3,
+    4663,
+    197494,
+    29144,
+    27.00,
+    55.00,
+    "https://www.greenmangaming.com/games/red-dead-redemption-2-pc/"
+);
