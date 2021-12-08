694415859ef98ec29b6e812c2bfaa8952ae514be
Kaung Myat Zaw <zaw@purdue.edu>
2021-12-08 04:05:45 -0500

    Connecting frontend to backend for adding games

--- routes/games.js
+++ routes/games.js
@@ -10,20 +10,6 @@ connection.connect(function (err) {
 
 router.post("/", (req, res) => {
   console.log("POST Request to /games");
-  // console.log(req.body.title);
-  // console.log(req.body.price);
-  // console.log(req.body.publisher);
-  // console.log(req.body.trailerLink);
-  // console.log(req.body.imgLink);
-  // console.log(req.body.criticScore);
-  // console.log(req.body.criticScoreCount);
-  // console.log(req.body.userScore);
-  // console.log(req.body.userScoreCount);
-  // console.log(req.body.positiveCount);
-  // console.log(req.body.negativeCount);
-  // console.log(req.body.discountPrice);
-  // console.log(req.body.discountPercent);
-  // console.log(req.body.website);
 
   connection.query(
     `
